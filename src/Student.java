679a8ad88ef2081f76beedc7a2ed8f8e7e0b8ce0
123 <bandidos-ice@mail.ru>
2021-12-24 17:13:51 +0300

    Chapter_3 initial commit A_1 - create sortBy: faculty, sort, birthDate, group

---
+++ src/Student.java
@@ -0,0 +1,119 @@
+/*
+Создать классы, спецификации которых приведены ниже.
+Определить конструкторы и методы setТип(), getТип(), toString().
+Определить дополнительно методы в классе, создающем массив объектов.
+Задать критерий выбора данных и вывести эти данные на консоль.
+В каждом классе, обладающем информацией, должно быть объявлено несколько конструкторов.
+
+1. Student: id, Фамилия, Имя, Отчество, Дата рождения, Адрес, Телефон,
+Факультет, Курс, Группа.
+Создать массив объектов. Вывести:
+a) список студентов заданного факультета;
+b) списки студентов для каждого факультета и курса;
+c) список студентов, родившихся после заданного года;
+d) список учебной группы.
+ */
+
+public class Student {
+    String id;
+    String surName;
+    String name;
+    String patronymic;
+    String birthDate;
+    String address;
+    String phoneNumber;
+    String faculty;
+    String course;
+    String group;
+
+    public Student(String id, String surName, String name, String patronymic, String birthDate,
+                   String address, String phoneNumber, String faculty, String course, String group) {
+        this.id = id;
+        this.surName = surName;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.faculty = faculty;
+        this.course = course;
+        this.group = group;
+    }
+
+    void sortByFaculty(String faculty) {
+        if (faculty.equals(this.faculty)) {
+            System.out.printf("Cтуденты из %s факультета %n" +
+                            "Student id: %s%n" +
+                            "Sur name: %s%n" +
+                            "Name: %s%n" +
+                            "Patronymic: %s%n" +
+                            "BirthDate: %s%n" +
+                            "Address: %s%n" +
+                            "PhoneNumber: %s%n" +
+                            "Faculty: %s%n" +
+                            "Cource: %s%n" +
+                            "Group: %s%n",
+                    faculty, this.id, this.surName, this.name, this.patronymic, this.birthDate, this.address,
+                    this.phoneNumber, this.faculty, this.course, this.group);
+            System.out.println("-------------------");
+        }
+    }
+
+    void sortByFacultyAndCource(String faculty, String course) {
+        if (faculty.equals(this.faculty) && course.equals(this.course)) {
+
+            System.out.printf("Соответсвующие критериям %s факультет и %s курс : %n" +
+                            "Student id: %s%n" +
+                            "Sur name: %s%n" +
+                            "Name: %s%n" +
+                            "Patronymic: %s%n" +
+                            "BirthDate: %s%n" +
+                            "Address: %s%n" +
+                            "PhoneNumber: %s%n" +
+                            "Faculty: %s%n" +
+                            "Cource: %s%n" +
+                            "Group: %s%n",
+                    faculty, course, this.id, this.surName, this.name, this.patronymic, this.birthDate, this.address,
+                    this.phoneNumber, this.faculty, this.course, this.group);
+            System.out.println("-------------------");
+        }
+    }
+
+    void sortByBirthDate(String birthDate) {
+        if (Integer.parseInt(birthDate) < Integer.parseInt(this.birthDate)) {
+            System.out.printf("Родились после %s %n" +
+                            "Student id: %s%n" +
+                            "Sur name: %s%n" +
+                            "Name: %s%n" +
+                            "Patronymic: %s%n" +
+                            "BirthDate: %s%n" +
+                            "Address: %s%n" +
+                            "PhoneNumber: %s%n" +
+                            "Faculty: %s%n" +
+                            "Cource: %s%n" +
+                            "Group: %s%n",
+                    birthDate, this.id, this.surName, this.name, this.patronymic, this.birthDate, this.address,
+                    this.phoneNumber, this.faculty, this.course, group);
+            System.out.println("-------------------");
+        }
+    }
+
+    void sortByGroup(String group) {
+        if (group.equals(this.group)) {
+            System.out.printf("Студенты из группы %s%n" +
+                            "Student id: %s%n" +
+                            "Sur name: %s%n" +
+                            "Name: %s%n" +
+                            "Patronymic: %s%n" +
+                            "BirthDate: %s%n" +
+                            "Address: %s%n" +
+                            "PhoneNumber: %s%n" +
+                            "Faculty: %s%n" +
+                            "Cource: %s%n" +
+                            "Group: %s%n",
+                    group, this.id, this.surName, this.name, this.patronymic, this.birthDate, this.address,
+                    this.phoneNumber, this.faculty, this.course, this.group);
+            System.out.println("-------------------");
+        }
+    }
+}
