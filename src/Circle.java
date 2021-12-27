6d9b25073dd0c7c9e415ad17df4efda651f263ce
123 <bandidos-ice@mail.ru>
2021-12-27 19:39:42 +0300

    B_9 - Circle.class - fix min\max finding method

---
+++ src/Circle.java
@@ -0,0 +1,111 @@
+/*
+Определить класс Окружность на плоскости. Определить площадь и периметр.
+Создать массив/список/множество объектов и определить группы
+окружностей, центры которых лежат на одной прямой.
+Определить наибольший и наименьший по площади (периметру) объект.
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Circle {
+    int id;
+    double radius;
+    double x;
+    double y;
+
+    public Circle(int id, double radius, double x, double y) {
+        this.id = id;
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(1, 1, 0, 5);
+        Circle circle2 = new Circle(2, 2, 0, 4);
+        Circle circle3 = new Circle(3, 3, 3, 4);
+        Circle circle4 = new Circle(4, 4, 3, 5);
+
+        Circle[] circles = new Circle[4];
+        circles[0] = circle;
+        circles[1] = circle2;
+        circles[2] = circle3;
+        circles[3] = circle4;
+        ArrayList<Double> area = new ArrayList<>();
+
+        area(circles, area);
+        circleLength(circles, area);
+        onTheSameLine(circles);
+        maxAndMinSqrt(area);
+    }
+
+    static void area(Circle[] circles, ArrayList<Double> area) {
+        System.out.println("-----------------");
+        final double PI = Math.PI;
+        for (Circle circle : circles) {
+            double result = PI * (circle.radius * circle.radius);
+            area.add(result);
+            System.out.printf("Площадь окружности №%s равна %s%n", circle.id, result);
+        }
+    }
+
+    static void circleLength(Circle[] circles, ArrayList<Double> area) {
+        System.out.println("-----------------");
+        final double PI = Math.PI;
+        for (Circle circle : circles) {
+            double result = 2 * (PI * (circle.radius));
+            area.add(result);
+            System.out.printf("Длинна окружности №%s равна %s%n", circle.id, result);
+        }
+    }
+
+    static void onTheSameLine(Circle[] circles) {
+        System.out.println("-----------------");
+        for (int i = 1; i < circles.length; i++) {
+            if (circles[i].x == circles[i - 1].x || circles[i].x == circles[i].y) {
+                System.out.printf("Окружность №%s c центром в точке [X:%s,Y:%s] лежит на одной прямой с окружностью №%s" +
+                        " c центром в точке [X:%s,Y:%s] %n", circles[i].id, circles[i].x, circles[i].y, circles[i].id, circles[i].x, circles[i].y);
+            }
+        }
+    }
+
+    static void maxAndMinSqrt(ArrayList<Double> area) {
+        System.out.println("---------------------");
+        System.out.println("Группа окружности");
+        System.out.printf("Min area : %s; %n", Collections.min(area));
+        System.out.printf("Max area : %s; %n", Collections.max(area));
+    }
+}
