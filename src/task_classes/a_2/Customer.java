84af764427571c9d88fed4ab1ff11a97f38936e8
Ilya Sikirytski <ilyasikirytski@gmail.com>
2021-12-28 09:41:24 +0300

    A_2 - TODO task done. Extract redundant code.

--- src/task_classes/a_2/Customer.java
+++ src/task_classes/a_2/Customer.java
@@ -1,11 +1,9 @@
-// Customer: id, Фамилия, Имя, Отчество, Адрес, Номер кредитной карточки,
+package task_classes.a_2;// task_classes.a_2.Customer: id, Фамилия, Имя, Отчество, Адрес, Номер кредитной карточки,
 //Номер банковского счета.
 //Создать массив объектов. Вывести:
 //a) список покупателей в алфавитном порядке;
 //b) список покупателей, у которых номер кредитной карточки находится
 //в заданном интервале.
-
-import java.util.Arrays;
 
 public class Customer {
     String id;
@@ -91,52 +89,12 @@ public class Customer {
 
     @Override
     public String toString() {
-        return "Customer id - " + id +
+        return "id - " + id +
                 " Sur Name - " + surName +
                 " Name - " + name +
                 " Patronymic - " + patronymic +
                 " Address - " + address +
                 " Card Number - " + cardNumber +
                 " Bank Number - " + bankNumber;
-    }
-
-    // TODO вынести в отдельный класс все что не нужно customer
-    public static void main(String[] args) {
-        Customer customer = new Customer("1", "1", "D", "1", "1", 1, "1");
-        Customer customer2 = new Customer("2", "2", "C", "2", "2", 2, "2");
-        Customer customer3 = new Customer("3", "3", "B", "3", "3", 3, "3");
-        Customer customer4 = new Customer("4", "4", "A", "4", "4", 4, "4");
-        Customer[] arrOfCustomers = new Customer[4];
-        String[] names = new String[arrOfCustomers.length];
-        arrOfCustomers[0] = customer;
-        arrOfCustomers[1] = customer2;
-        arrOfCustomers[2] = customer3;
-        arrOfCustomers[3] = customer4;
-
-        alphabetSort(arrOfCustomers, names);
-        bankCardInRange(arrOfCustomers, 0, 5);
-    }
-    static void alphabetSort(Customer[] arrOfCustomers, String[] names){
-        System.out.println("Клиенты отсортированные в алфавитном порядке");
-        for (int i = 0; i < arrOfCustomers.length; i++) {
-            names[i] = arrOfCustomers[i].name;
-        }
-        Arrays.sort(names);
-        for (String name : names) {
-            for (Customer customers : arrOfCustomers) {
-                if (name.equals(customers.name)) {
-                    System.out.println(customers);
-                }
-            }
-        }
-    }
-
-    static void bankCardInRange(Customer[] arrOfCustomers, int rangeFrom, int rangeTo){
-        System.out.printf("Клиенты, банковские карты которых входят в диапазон от %s до %s%n", rangeFrom, rangeTo);
-        for (Customer customers : arrOfCustomers) {
-            if (customers.cardNumber > rangeFrom && customers.cardNumber < rangeTo) {
-                System.out.println(customers);
-            }
-        }
     }
 }
---
+++ src/task_classes/a_2/Main_A_2.java
@@ -0,0 +1,45 @@
+package task_classes.a_2;
+
+import java.util.Arrays;
+
+public class Main_A_2 {
+    public static void main(String[] args) {
+        Customer customer = new Customer("1", "1", "D", "1", "1", 1, "1");
+        Customer customer2 = new Customer("2", "2", "C", "2", "2", 2, "2");
+        Customer customer3 = new Customer("3", "3", "B", "3", "3", 3, "3");
+        Customer customer4 = new Customer("4", "4", "A", "4", "4", 4, "4");
+        Customer[] arrOfCustomers = new Customer[4];
+        String[] names = new String[arrOfCustomers.length];
+        arrOfCustomers[0] = customer;
+        arrOfCustomers[1] = customer2;
+        arrOfCustomers[2] = customer3;
+        arrOfCustomers[3] = customer4;
+
+        alphabetSort(arrOfCustomers, names);
+        bankCardInRange(arrOfCustomers, 0, 5);
+    }
+
+    static void alphabetSort(Customer[] arrOfCustomers, String[] names) {
+        System.out.println("Клиенты отсортированные в алфавитном порядке");
+        for (int i = 0; i < arrOfCustomers.length; i++) {
+            names[i] = arrOfCustomers[i].name;
+        }
+        Arrays.sort(names);
+        for (String name : names) {
+            for (Customer customers : arrOfCustomers) {
+                if (name.equals(customers.name)) {
+                    System.out.println(customers);
+                }
+            }
+        }
+    }
+
+    static void bankCardInRange(Customer[] arrOfCustomers, int rangeFrom, int rangeTo) {
+        System.out.printf("Клиенты, банковские карты которых входят в диапазон от %s до %s%n", rangeFrom, rangeTo);
+        for (Customer customers : arrOfCustomers) {
+            if (customers.cardNumber > rangeFrom && customers.cardNumber < rangeTo) {
+                System.out.println(customers);
+            }
+        }
+    }
+}
