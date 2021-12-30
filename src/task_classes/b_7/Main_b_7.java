7e365977a4c66dde8fff6e40b4865527415f9f0a
Ilya Sikirytski <ilyasikirytski@gmail.com>
2021-12-30 09:10:16 +0300

    B_7 - TODO task done

--- src/task_classes/b_7/Main_b_7.java
+++ src/task_classes/b_7/Main_b_7.java
@@ -2,113 +2,113 @@ package task_classes.b_7;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 import static java.lang.Math.sqrt;
 
 public class Main_b_7 {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle(1, 1, 1);
-        Triangle triangle2 = new Triangle(2, 2, 2);
-        Triangle triangle3 = new Triangle(3, 3.55, 3.45);
-        Triangle triangle4 = new Triangle(4, 4.55, 4.45);
+        Triangle[] triangles = new Triangle[]{
+                new Triangle(1, 1, 1),
+                new Triangle(2, 2, 2),
+                new Triangle(3, 3, 4),
+                new Triangle(2, 2, 4)
+        };
 
-        Triangle[] triangles = new Triangle[4];
-        triangles[0] = triangle;
-        triangles[1] = triangle2;
-        triangles[2] = triangle3;
-        triangles[3] = triangle4;
-        ArrayList<Triangle> equilateral = new ArrayList<>();
-        ArrayList<Triangle> isosceles = new ArrayList<>();
-        ArrayList<Triangle> unequal = new ArrayList<>();
-        ArrayList<Double> equilateralDouble = new ArrayList<>();
-        ArrayList<Double> isoscelesDouble = new ArrayList<>();
-        ArrayList<Double> unequalDouble = new ArrayList<>();
-        Double[] perimeter = new Double[triangles.length];
+        ArrayList<Triangle> groupOfEquilaterals = new ArrayList<>();
+        ArrayList<Triangle> groupOfIsosceles = new ArrayList<>();
+        ArrayList<Triangle> groupOfUnequals = new ArrayList<>();
 
-        squareHeron(triangles);
-        perimeter(triangles, perimeter);
-        groupOfTriangle(triangles, equilateralDouble, isoscelesDouble, unequalDouble, equilateral, isosceles, unequal);
-        numberOfGroupOfTriangles(equilateral, isosceles, unequal);
-        maxAndMinSqrt(equilateralDouble, isoscelesDouble, unequalDouble);
+        findingSquare(triangles);
+        findingPerimeter(triangles);
+        groupingOfTriangles(triangles, groupOfEquilaterals, groupOfIsosceles, groupOfUnequals);
+        numberOfTrianglesInGroup(groupOfEquilaterals, groupOfIsosceles, groupOfUnequals);
+        maxAndMinSquare(groupOfEquilaterals, groupOfIsosceles, groupOfUnequals);
     }
 
-    // TODO названия методов с глаголом
-    // TODO площадь в отдельный метод, лучше в Triangle
-    static void squareHeron(Triangle[] triangles) {
+    static double square(Triangle triangle) {
+        return sqrt(triangle.perimeter() * (triangle.perimeter() - triangle.getA()) * (triangle.perimeter() - triangle.getB()) * (triangle.perimeter() - triangle.getC()));
+    }
+
+    // DO названия методов с глаголом
+    // DO площадь в отдельный метод, лучше в Triangle
+    static void findingSquare(Triangle[] triangles) {
         System.out.println("---------------------");
         for (int i = 0; i < triangles.length; i++) {
-            double p = (triangles[i].a + triangles[i].b + triangles[i].c) / 2;
-            double square = sqrt(p * (p - triangles[i].a) * (p - triangles[i].b) * (p - triangles[i].c));
-            System.out.printf("Площадь треугольника номер: %s равна: %s%n", i, square);
+            System.out.printf("Площадь треугольника номер: %s равна: %s%n", i, square(triangles[i]));
         }
     }
 
-    // TODO названия методов с глаголом
-    static void perimeter(Triangle[] triangles, Double[] perimeter) {
+    // DO названия методов с глаголом
+    static void findingPerimeter(Triangle[] triangles) {
         System.out.println("---------------------");
         for (int i = 0; i < triangles.length; i++) {
-            double perim = triangles[i].a + triangles[i].b + triangles[i].c;
-            perimeter[i] = perim;
-            System.out.printf("Периметр треугольника номер: %s равен: %s%n", i, perimeter[i]);
+            System.out.printf("Периметр треугольника номер: %s равен: %s%n", i, triangles[i].perimeter());
         }
     }
 
-    // TODO названия методов с глаголом
-    // TODO периметр в отдельный метод, лучше в Triangle
-    // TODO вывод на экран и группировку разделить на 2 метода
-    // TODO вынести определение типа треугольника в отдельный метод
-    static void groupOfTriangle(Triangle[] triangles, ArrayList<Double> equilateralDouble, ArrayList<Double> isoscelesDouble, ArrayList<Double> unequalDouble, ArrayList<Triangle> equilateral, ArrayList<Triangle> isosceles, ArrayList<Triangle> unequal) {
+    static void print(Triangle t, String str) {
+        System.out.println("Треугольник со сторонами {" +
+                "a=" + t.getA() +
+                ", b=" + t.getB() +
+                ", c=" + t.getC() +
+                '}' + " - " + str);
+    }
+
+    // DO названия методов с глаголом
+    // DO периметр в отдельный метод, лучше в Triangle
+    // DO вывод на экран и группировку разделить на 2 метода
+    // DO вынести определение типа треугольника в отдельный метод
+    static void groupingOfTriangles(Triangle[] triangles, ArrayList<Triangle> groupOfEquilaterals, ArrayList<Triangle> groupOfIsosceles, ArrayList<Triangle> groupOfUnequals) {
         System.out.println("---------------------");
-        for (int i = 0; i < triangles.length; i++) {
-            if (triangles[i].a == triangles[i].b && triangles[i].b == triangles[i].c) {
-                equilateral.add(triangles[i]);
-                equilateralDouble.add(triangles[i].a + triangles[i].b + triangles[i].c);
-                System.out.printf("Треугольник № %s - Равносторонний %n", i);
-            } else if (triangles[i].a == triangles[i].b || triangles[i].b == triangles[i].c || triangles[i].c == triangles[i].a) {
-                isosceles.add(triangles[i]);
-                isoscelesDouble.add(triangles[i].a + triangles[i].b + triangles[i].c);
-                System.out.printf("Треугольник № %s - Равнобедренный %n", i);
-            } else if (triangles[i].a != triangles[i].b || triangles[i].b != triangles[i].c || triangles[i].c != triangles[i].a) {
-                unequal.add(triangles[i]);
-                unequalDouble.add(triangles[i].a + triangles[i].b + triangles[i].c);
-                System.out.printf("Треугольник № %s - Прямоугольный %n", i);
+        for (Triangle t : triangles) {
+            if (t.isNonExistent()) {
+                print(t, "Не существует");
+            } else if (t.groupingOfEquilaterals(t, groupOfEquilaterals)) {
+                print(t, "Равносторонний");
+            } else if (t.groupingOfUnequals(t, groupOfUnequals)) {
+                print(t, "Прямоугольный");
+            } else if (t.groupingOfIsosceles(t, groupOfIsosceles)) {
+                print(t, "Равнобедренный");
             }
         }
     }
 
-    // TODO названия методов с глаголом
-    static void numberOfGroupOfTriangles(ArrayList<Triangle> equilateral, ArrayList<Triangle> isosceles, ArrayList<Triangle> unequal) {
+    // DO названия методов с глаголом
+    static void numberOfTrianglesInGroup(ArrayList<Triangle> groupOfEquilaterals, ArrayList<Triangle> groupOfIsosceles, ArrayList<Triangle> groupOfUnequals) {
         System.out.println("---------------------");
-        if (equilateral != null) {
-            System.out.printf("Кол-во равнобедренных трегольников - %s%n", isosceles.size());
+        if (groupOfEquilaterals != null) {
+            System.out.printf("Кол-во равносторонних трегольников - %s%n", groupOfEquilaterals.size());
         }
-        if (isosceles != null) {
-            System.out.printf("Кол-во равносторонних трегольников - %s%n", equilateral.size());
+        if (groupOfIsosceles != null) {
+            System.out.printf("Кол-во равнобедренных трегольников - %s%n", groupOfIsosceles.size());
         }
-        if (unequal != null) {
-            System.out.printf("Кол-во прямоугольных трегольников - %s%n", unequal.size());
+        if (groupOfUnequals != null) {
+            System.out.printf("Кол-во прямоугольных трегольников - %s%n", groupOfUnequals.size());
         }
     }
 
-    // TODO названия методов с глаголом
-    // TODO убрать ArrayList<Double>, использовать comparable или comparator
-    static void maxAndMinSqrt(ArrayList<Double> equilateralDouble, ArrayList<Double> isoscelesDouble, ArrayList<Double> unequalDouble) {
+    // DO названия методов с глаголом
+    // DO убрать ArrayList<Double>, использовать comparable или comparator
+    static void maxAndMinSquare(ArrayList<Triangle> groupOfEquilaterals, ArrayList<Triangle> groupOfIsosceles, ArrayList<Triangle> groupOfUnequals) {
         System.out.println("---------------------");
         System.out.println("Группа равносторонние");
-        for (int i = 1; i < equilateralDouble.size(); i++) {
-            System.out.printf("Min perimeter : %s; %n", Collections.min(equilateralDouble));
-            System.out.printf("Max perimeter : %s; %n", Collections.max(equilateralDouble));
+        if (!groupOfEquilaterals.isEmpty()) {
+            System.out.printf("Min perimeter : %s; %n", Collections.min(groupOfEquilaterals));
+            System.out.printf("Max perimeter : %s; %n", Collections.max(groupOfEquilaterals));
+        } else {
+            System.out.println("Нет элементов");
         }
         System.out.println("Группа равнобедренные");
-        for (int i = 1; i < isoscelesDouble.size(); i++) {
-            System.out.printf("Min perimeter : %s; %n", Collections.min(isoscelesDouble));
-            System.out.printf("Max perimeter : %s; %n", Collections.max(isoscelesDouble));
+        if (!groupOfIsosceles.isEmpty()) {
+            System.out.printf("Min perimeter : %s; %n", Collections.min(groupOfIsosceles));
+            System.out.printf("Max perimeter : %s; %n", Collections.max(groupOfIsosceles));
+        } else {
+            System.out.println("Нет элементов");
         }
         System.out.println("Группа прямоугольные");
-        for (int i = 1; i < unequalDouble.size(); i++) {
-            System.out.printf("Min perimeter : %s; %n", Collections.min(unequalDouble));
-            System.out.printf("Max perimeter : %s; %n", Collections.max(unequalDouble));
+        if (!groupOfUnequals.isEmpty()) {
+            System.out.printf("Min perimeter : %s; %n", Collections.min(groupOfUnequals));
+            System.out.printf("Max perimeter : %s; %n", Collections.max(groupOfUnequals));
         }
     }
 }
--- src/task_classes/b_7/Triangle.java
+++ src/task_classes/b_7/Triangle.java
@@ -1,5 +1,7 @@
 package task_classes.b_7;
 
+import java.util.ArrayList;
+
 /*
 Определить класс Треугольник на плоскости.
 Определить площадь и периметр треугольника.
@@ -7,10 +9,10 @@ package task_classes.b_7;
 треугольников разного типа (равносторонний, равнобедренный, прямоугольный, произвольный).
 Определить для каждой группы наибольший и наименьший по площади (периметру) объект
  */
-public class Triangle {
-    double a;
-    double b;
-    double c;
+public class Triangle implements Comparable<Triangle> {
+    private double a;
+    private double b;
+    private double c;
 
     public Triangle(double a, double b, double c) {
         this.a = a;
@@ -26,7 +28,7 @@ public class Triangle {
         this.a = a;
     }
 
-    public double getD() {
+    public double getB() {
         return b;
     }
 
@@ -40,6 +42,75 @@ public class Triangle {
 
     public void setC(double c) {
         this.c = c;
+    }
+
+    boolean isEquilateral() {
+        return a == b && b == c;
+    }
+
+    boolean isIsosceles() {
+        return a == b || b == c || c == a;
+    }
+
+    boolean isUnequals() {
+        return a + b == c || b + c == a;
+    }
+
+    boolean isNonExistent() {
+        return a + b < c || a + c < b || b + c < a;
+    }
+
+    boolean groupingOfEquilaterals(Triangle triangles, ArrayList<Triangle> groupOfEquilaterals) {
+        boolean x = false;
+        if (triangles.isEquilateral()) {
+            groupOfEquilaterals.add(triangles);
+            x = true;
+        }
+        return x;
+    }
+
+    boolean groupingOfIsosceles(Triangle triangles, ArrayList<Triangle> groupOfIsosceles) {
+        boolean x = false;
+        if (triangles.isIsosceles()) {
+            groupOfIsosceles.add(triangles);
+            x = true;
+        }
+        return x;
+    }
+
+    boolean groupingOfUnequals(Triangle triangles, ArrayList<Triangle> groupOfUnequals) {
+        boolean x = false;
+        if (triangles.isUnequals()) {
+            groupOfUnequals.add(triangles);
+            x = true;
+        }
+        return x;
+    }
+
+    double perimeter() {
+        return (a + b + c);
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}' + " - " + perimeter();
+    }
+
+    @Override
+    public int compareTo(Triangle o) {
+        double firstPerimeter = perimeter();
+        double secondPerimeter = o.perimeter();
+        if (firstPerimeter > secondPerimeter) {
+            return 1;
+        } else if (firstPerimeter == secondPerimeter) {
+            return 0;
+        } else if (firstPerimeter < secondPerimeter) {
+            return -1;
+        } else return 2;
     }
 }
 
